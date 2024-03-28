55c289e5057982052e3140a69e639f98f513803c
Joshua Johnson <joshuahjohnson@gmail.com>
2024-03-28 16:02:35 -0600

    Added ALGORITHM to env var in main.py

--- webdev/main.py
+++ webdev/main.py
@@ -17,6 +17,7 @@ from webdev.database import startup, get_database, get_user, create_user, get_se
 from webdev.database import Token, TokenData, User, UserInDB, store_file_metadata, get_file_metadata
 
 os.environ['ACCESS_TOKEN_EXPIRE_MINUTES']='15'
+os.environ['ALGORITHM']='HS256'
 # Load the env configuration
 load_dotenv()
 
