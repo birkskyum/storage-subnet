60036d1e9522a7b8211156519aa08915d12ba323
Joshua Johnson <joshuahjohnson@gmail.com>
2024-03-28 15:36:38 -0600

    Added env var ACCESS_TOKEN_EXPIRE_MINUTES to main.py

--- webdev/main.py
+++ webdev/main.py
@@ -16,6 +16,7 @@ from storage.api import StoreUserAPI, RetrieveUserAPI, get_query_api_axons
 from webdev.database import startup, get_database, get_user, create_user, get_server_wallet, get_metagraph
 from webdev.database import Token, TokenData, User, UserInDB, store_file_metadata, get_file_metadata
 
+os.environ['ACCESS_TOKEN_EXPIRE_MINUTES']=15
 # Load the env configuration
 load_dotenv()
 
