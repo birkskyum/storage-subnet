7d862a24d416b299dd2a5b03bb14f3fc074fbbeb
Ayden Brewer <dalegribble@riseup.net>
2024-04-10 15:01:14 -0700

    Fix import

--- neurons/api.py
+++ neurons/api.py
@@ -41,7 +41,7 @@ from storage.validator.database import retrieve_encryption_payload, get_ordered_
 from storage.validator.cid import generate_cid_string
 from storage.validator.encryption import decrypt_data_with_private_key
 
-from .api.indexer import run_indexer_thread
+from .indexer import run_indexer_thread
 
 
 def MockDendrite():
